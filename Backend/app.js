e55900f57c17e0e93b92bdd9c193bfe4fa953e9c
Unish215638 <unish215638@gmail.com>
2019-09-18 08:10:32 +0545

    Events crud finished

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 require('./Database/database');
 const userModel=require('./Model/userModel');
 const adminModel=require('./Model/adminModel');
+const eventModel=require('./Model/adminEventModel');
 const adminAuth=require('./MiddleWare/adminAuth');
 const express=require('express');
 const bodyparser=require('body-parser');
@@ -35,6 +36,66 @@ app.post("/userRequest", (req, res) => {
     console.log("en");
     console.log(token);  
    })
+
+ //API for adding Event
+app.post("/addEvent",(req, res) => {
+    console.log(req.body);
+    var eventData = new eventModel(req.body);
+    eventData.save().then(function(){
+        // res.send('fine');
+        alert("Event Added");
+        console.log('Event info added');
+    }).catch(function(e){res.send(e) });
+});
+
+
+
+ //API for viewing all events
+ app.get("/showEventDetails",function(req,res){
+  eventModel.find().then(function(eventModel){
+      console.log(eventModel);
+      res.send(eventModel);
+  }).catch(function(e){
+      res.send(e);
+  })
+})
+
+//  API for viewing specific events
+app.get('/showSpecificEvent/:id', function (req, res) {    
+  eid = req.params.id.toString();
+  eventModel.findById(eid).then(function (eventModel) {
+      res.send(eventModel);
+  }).catch(function (e) {
+      res.send(e)
+  });
+});
+
+// API for updating specific events
+app.put('/updateSpecificEvent/:id', function (req, res) {   //update productr
+  eid = req.params.id.toString();
+  console.log(eid);
+  console.log(req.body);
+
+  eventModel.findByIdAndUpdate(eid,req.body,{new: true}, (err,eventModel) => {
+      // Handle any possible database errors
+      res.send(eventModel);
+      });
+  });
+
+
+  
+        // API for deleting specific events
+        app.delete('/deleteSpecificEvent/:id', function (req, res) {    
+            
+          console.log(req.params.id);
+           eventModel.findByIdAndDelete(req.params.id).then(function(){
+               res.send("Successfully deleted");
+           }).catch(function(e){
+               res.send(e);
+           }) ;
+           });
+
+
 
  ///Server Port
  app.listen(96);
