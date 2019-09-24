d25bd74ffa6310848cee97d1eb5b14a99c4644f2
amarmahato <amarmahato712@gmail.com>
2019-09-24 20:38:48 +0545

    bugs fixes

--- Backend/Testing/teacherModel.test.js
+++ Backend/Testing/teacherModel.test.js
@@ -1,4 +1,4 @@
-const userModel=require('../Model/teacherModel');
+const teacherModel=require('../Model/teacherModel');
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/lmstesting';
 beforeAll(async () => {
@@ -14,28 +14,41 @@ beforeAll(async () => {
    // the code below is for insert testing
     it('Add product testing anything', () => {
     const user = {
-    'first_name': 'Amar',
-    'last_name': 'Shrestha',
+    'first_name': 'Jackson',
+    'last_name': 'Michael',
     'gender': 'Male',
     'user_type': 'admin',
-    'email': 'mahato@amar',
-    'contact': '981709786',
+    'email': 'michaeljackson@gmail',
+    'contact': '981709899',
     'password': 'admin'
     
     };
    
-    return userModel.create(user)
+    return teacherModel.create(user)
     .then((pro_ret) => {
-    expect(pro_ret.first_name).toEqual('Amar');
-    expect(pro_ret.last_name).toEqual('Mahato');
+    expect(pro_ret.first_name).toEqual('Jackson');
+    expect(pro_ret.last_name).toEqual('Michael');
     expect(pro_ret.gender).toEqual('Male');
     expect(pro_ret.user_type).toEqual('admin');
     expect(pro_ret.email).toEqual('mahato@amar');
-    expect(pro_ret.contact).toEqual('981709786');
+    expect(pro_ret.contact).toEqual('981709899');
     expect(pro_ret.password).toEqual('admin');
 
     });
     });
+
+
+    // update the test
+    it('to test the update', async () => { 
+ 
+        return teacherModel.updateOne({
+           _id :Object('5dc8cc9edeac4005bc0aed92'
+           )}, 
+           {$set : {first_name:'Dragonball'}})     
+            .then((pp)=>{         
+                expect(pp.ok).toEqual(1)     
+          })    
+        }) 
    // the code below is for delete testing
     it('to test the delete product is working or not', async () => {
      const status = await userModel.deleteMany();
--- Backend/Testing/userModel.test.js
+++ Backend/Testing/userModel.test.js
@@ -15,12 +15,12 @@ beforeAll(async () => {
     it('Add product testing anything', () => {
     const user = {
     'first_name': 'Amar',
-    'last_name': 'Shrestha',
+    'last_name': 'Mahato',
     'gender': 'Male',
     'user_type': 'admin',
     'email': 'mahato@amar',
     'contact': '981709786',
-    'password': 'admin'
+    'password': 'admin22'
     
     };
    
@@ -32,11 +32,24 @@ beforeAll(async () => {
     expect(pro_ret.user_type).toEqual('admin');
     expect(pro_ret.email).toEqual('mahato@amar');
     expect(pro_ret.contact).toEqual('981709786');
-    expect(pro_ret.password).toEqual('admin');
+    expect(pro_ret.password).toEqual('admin22');
 
     });
     });
-   // the code below is for delete testing
+
+
+// update the test
+    it('to test the update', async () => { 
+ 
+      return userModel.updateOne({
+         _id :Object('5dc8cc9edeac4005bc0aed92'
+         )}, 
+         {$set : {first_name:'Dragonball'}})     
+          .then((pp)=>{         
+              expect(pp.ok).toEqual(1)     
+        })    
+      }) 
+  // the code below is for delete testing
     it('to test the delete product is working or not', async () => {
      const status = await userModel.deleteMany();
     expect(status.ok).toBe(1);
