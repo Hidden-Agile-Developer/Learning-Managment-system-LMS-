7aa6e28a8d0e0c44aef3055d62ab988e2590abf3
Himanchalmalla <himanchalmalla001@gmail.com>
2019-11-27 22:59:51 +0545

    project updated

--- frontend/Jquery/student.js
+++ frontend/Jquery/student.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
    
   update_student_profile();
-
+  update_student();
+  var student_id;
   let user_image = '';
   $("#profile_image").on('change', function () {
        let formData = new FormData();
@@ -42,7 +43,7 @@ $.ajax({
                         "<td class='col-xs-2'>" + data[index].subject + "</td>"+
                         "<td class='col-xs-1'>" + data[index].semister + "</td>"+
                         "<td class='col-xs-1'>" + data[index].section + "</td>"+
-                        "<td class='col-xs-1'>"+ "<a href='profile.html?id="+data[index]._id+"' class='btn btn-primary btn-sm'>"+
+                        "<td class='col-xs-1'>"+ "<a href='adminstudentprofile.html?id="+data[index]._id+"' class='btn btn-primary btn-sm'>"+
                         "<i class='fa fa-edit'></a></td>"+
                         "<td class='col-xs-1'>"+ "<Button class='btn btn-primary btn-sm delete' student_id='"+data[index]._id+"'>"+
                         "<i class='fa fa-trash'></Button></td></tr>");
@@ -56,6 +57,7 @@ $.ajax({
       });
 
 function update_student_profile(){
+
   $("#update_student_profile").click(function(){
   var id2=$("#student_id").val();
   first_name=$("#first_name").val();
@@ -120,6 +122,91 @@ function update_student_profile(){
   });
   });
 
-}      
+}
+
+function update_student(){
+  var urlParams = new URLSearchParams(window.location.search);
+  var id = urlParams.get("id");
+  console.log(id);
+
+
+ $.getJSON("http://localhost:96/viewStudent/" + id, function (result) {
+     $("#first_name").val(result.first_name);
+     $("#last_name").val(result.last_name);
+     $("#gender").val(result.gender);
+     $("#subject").val(result.subject);
+     $("#semister").val(result.semister);
+     $("#section").val(result.section);
+     $("#email").val(result.email);
+     $("#contact").val(result.contact);
+     $("#password").val(result.password);
+     $("#ram").append( "<img src='http://localhost:96/images/"+result.profile_image+"' "+ "class='rounded-circle' style=width:150px;height:150px;>" )
+   
+      
+  });
+
+  
+
+     $("#update_student").click(function(){
+     first_name=$("#first_name").val();
+     last_name=$("#last_name").val();
+     gender=$("#gender").val();
+     subject=$("#subject").val();
+     semister=$("#semister").val();
+     section=$("#section").val();
+     email=$("#email").val();
+     contact=$("#contact").val();
+     password=$("#password").val();
+        
+     data={
+    "first_name":first_name,
+    "last_name":last_name,
+    "gender":gender,
+    "subject":subject,
+    "semister":semister,
+    "section":section,
+    "email":email,
+    "contact":contact,
+    "password":password,
+  }
+
+    $.ajax({
+    url:'http://localhost:96/updateStudent/' + id,
+    type:'PUT',
+    dataType:'json',
+    data:data,
+    success:function(res, textStatus, xhr){
+    alert('Student updated!!');     
+    console.log(res);          
+     },
+    error:function(xhr, textStatus, errorThrown){
+    alert('Error! to update Student');
+     }
+          
+    });
+    });
+/////////////image---update////////////////////
+$("#update_image").click(function(){
+  data={
+ "profile_image":userImage,
+}
+
+ $.ajax({
+ url:'http://localhost:96/updateStudent/' + id,
+ type:'PUT',
+ dataType:'json',
+ data:data,
+ success:function(res, textStatus, xhr){
+ alert('Profile image updated!!');    
+ location.href="studentadmin.html"; 
+ console.log(res);          
+  },
+ error:function(xhr, textStatus, errorThrown){
+ alert('Error! to update Student');
+  }
+       
+ });
+ });
+     }
 
 });
--- frontend/Jquery/teacher.js
+++ frontend/Jquery/teacher.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     update_teacher_profile();
+    update_teacher();
 
     let user_image = '';
     $("#profile_image").on('change', function () {
@@ -38,7 +39,7 @@ $("#table_teacher").append("<tr style'text-align:center'>"+
 "<td class='col-xs-2'>" + data[index].email +"</td>"+
 "<td class='col-xs-2'>" + data[index].contact + "</td>"+
 "<td class='col-xs-2'>" +"<img src='http://localhost:96/images/"+data[index].profile_image+"' "+ "class='img-rounded' style=width:50px;height:50px;>" + "</td>"+
-"<td class='col-xs-1'>"+ "<a href='teacherProfile.html?id="+data[index]._id+"' class='btn btn-primary btn-sm'>"+
+"<td class='col-xs-1'>"+ "<a href='adminteacherprofile.html?id="+data[index]._id+"' class='btn btn-primary btn-sm'>"+
 "<i class='fa fa-edit'></a></td>"+
 "<td class='col-xs-1'>"+ "<Button class='btn btn-primary btn-sm delete' teacher_id='"+data[index]._id+"'>"+
 "<i class='fa fa-trash'></Button></td>"+
@@ -112,6 +113,80 @@ function update_teacher_profile(){
 
 
 }
+
+function update_teacher(){
+ var urlParams = new URLSearchParams(window.location.search);
+ var id = urlParams.get("id");
+ console.log(id);
+
+
+ $.getJSON("http://localhost:96/viewTeacher/" + id, function (result) {
+  console.log(result._id);
+  $("#first_name").val(result.first_name);
+  $("#last_name").val(result.last_name);
+  $("#gender").val(result.gender);
+  $("#email").val(result.email);
+  $("#contact").val(result.contact);
+  $("#ram").append( "<img src='http://localhost:96/images/"+result.profile_image+"' "+ "class='rounded' style=width:150px;height:150px;>" )        
+ });
+
+
+  $("#update_teacher").click(function(){
+  first_name=$("#first_name").val();
+  last_name=$("#last_name").val();
+  gender=$("#gender").val();
+  email=$("#email").val();
+  contact=$("#contact").val();
+   
+          
+ data={
+   "first_name":first_name,
+   "last_name":last_name,
+   "gender":gender,
+   "email":email,
+   "contact":contact,
+ }
+
+ $.ajax({
+   url:'http://localhost:96/updateTeacher/' + id,
+   type:'PUT',
+   dataType:'json',
+   data:data,
+   success:function(res, textStatus, xhr){
+   alert('Teacher updated!!');
+   location.href="adminteacherprofile.html";     
+   console.log(res);          
+ },
+   error:function(xhr, textStatus, errorThrown){
+   alert('Error! to update Teacher');
+ }
+           
+ });
+ });
+    
+
+ $("#update_image").click(function(){
+   data={
+   "profile_image":userImage,
+ }
+      
+   $.ajax({
+   url:'http://localhost:96/updateTeacher/' + id,
+   type:'PUT',
+   dataType:'json',
+   data:data,
+   success:function(res, textStatus, xhr){
+   alert('Profile image updated!!');    
+   location.href="teacheradmin.html"; 
+   console.log(res);          
+ },
+    error:function(xhr, textStatus, errorThrown){
+    alert('Error! to update Student');
+    }
+             
+ });
+ });
+ }
 
 function delete_teacher(){
 
